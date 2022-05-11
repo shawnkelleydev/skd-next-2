d17dd3d0fd4a0f88fdd4636c96c196dea5b0f1e0
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-11 16:24:04 -0600

    Add photos and tech svgs

---
+++ components/TechSVG/index.js
@@ -0,0 +1,11 @@
+import CSS from './CSS'
+import Git from './Git'
+import GitHub from './GitHub'
+import HTML from './HTML'
+import JS from './JS'
+import Next from './Next'
+import React from './React'
+import Router from './Router'
+import VSCode from './VSCode'
+
+export default { HTML, CSS, JS, React, Router, Next, Git, GitHub, VSCode }
