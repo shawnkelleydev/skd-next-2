34cd0aec69ed86684c9ef6624acf7caabee135af
Shawn Kelley <skelley@us.ci.org>
2022-05-17 08:49:01 -0600

    Refactor DownArrow

--- components/ArrowDown/index.js
+++ components/ArrowDown/index.js
@@ -1,38 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import styles from './styles.module.css'
 
 export default function ArrowDown() {
-  const [anchor] = useState('')
-  const [hideArrow, setHideArrow] = useState(true)
   const [y, setY] = useState(0)
 
   const [windowHeight, setWindowHeight] = useState(1000)
+  const [pageHeight, setPageHeight] = useState(3000)
+
+  const ref = useRef(null)
 
   useEffect(() => {
     setWindowHeight(window.innerHeight)
-    window.addEventListener('resize', () => setWindowHeight(window.innerHeight))
+    window.addEventListener('resize', () => {
+      setWindowHeight(window.innerHeight)
+
+      const main = document.querySelector('main')
+      setPageHeight(main.scrollHeight)
+    })
   }, [])
-  useEffect(() => {
-    window.addEventListener('scroll', () => setY(window.pageYOffset))
-  }, [anchor])
 
   useEffect(() => {
-    if (y > 50) setHideArrow(true)
-    if (y < 50) setHideArrow(false)
-  }, [hideArrow, y])
+    window.addEventListener('scroll', () => setY(window.scrollY))
+  }, [])
 
   const handleClick = () => {
-    scroll({ top: windowHeight, left: 0, behavior: 'smooth' })
+    scroll({ top: y + windowHeight, left: 0, behavior: 'smooth' })
   }
 
   return (
     <div
       className={styles['arrow-down']}
-      data-hide-arrow={hideArrow}
-      onClick={() => {
-        if (!hideArrow) handleClick()
-      }}
+      data-hide-arrow={false}
+      onClick={handleClick}
+      ref={ref}
     >
       <svg
         xmlns='http://www.w3.org/2000/svg'
