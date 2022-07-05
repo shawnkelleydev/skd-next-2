1b8bf39f68ea6a252e38e186d45cb01bb58303b8
Shawn Kelley <skelley@us.ci.org>
2022-07-05 10:45:32 -0600

    Remove config from settings.json

--- components/Banner/Buttons/index.js
+++ components/Banner/Buttons/index.js
@@ -2,22 +2,22 @@ import Button from 'components/Button'
 import LinkedIn from 'components/LinkedIn'
 
 export default function Buttons() {
-    return (
-        <span data-hide={state.step < 8}>
-            <Button
-                disabled={state.step < 8}
-                url='https://github.com/shawnkelleydev/skd-next-2'
-            >
-                see the repo
-            </Button>
+  return (
+    <span data-hide={state.step < 8}>
+      <Button
+        disabled={state.step < 8}
+        url='https://github.com/shawnkelleydev/skd-next-2'
+      >
+        see the repo
+      </Button>
 
-            <a
-                href='https://www.linkedin.com/in/shawnkelleydev/'
-                rel='noreferrer'
-                target='_blank'
-            >
-                <LinkedIn />
-            </a>
-        </span>
-    )
+      <a
+        href='https://www.linkedin.com/in/shawnkelleydev/'
+        rel='noreferrer'
+        target='_blank'
+      >
+        <LinkedIn />
+      </a>
+    </span>
+  )
 }
